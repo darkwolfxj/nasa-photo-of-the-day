836adb93ce6118a5056d4240b5d6bf7da8a1e0fc
darkwolfxj <hesitation.dotdot@gmail.com>
2020-01-22 18:52:55 -0500

    mvp

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
 <>
     <div className="App">
-      <p>NASA APOD</p>
+      <p>NASA Astronomy Photo of the Day</p>
     </div>
     <PhotoRender />
 </>
--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,22 +1,18 @@
-import React from 'react';
+import React from "react";
 
 export default function Photo(props) {
-
-    return (
-        <>
-            <p className='headline'>Astronomy Photo of the Day</p>
-            <img className='photo' src={props.data.hdurl} alt='nasa apod'/>
-            <div className="copyright-caption">
-                <div className="copyright-date">
-                    <p>{props.data.copyright}</p>
-                    <p>{props.data.date}</p>
-                </div>
-                <div className='caption'>
-                    <p>{props.data.explanation}</p>
-                </div>
-            </div>
-
-        </>
-    )
+	return (
+		<>
+			<p className="headline"> {props.data.title} </p>{" "}
+			<img className="photo" src={props.data.hdurl} alt="nasa apod" />
+			<div className="copyright-caption">
+				<div className="copyright-date">
+					<p> {props.data.copyright} </p> <p> {props.data.date} </p>{" "}
+				</div>{" "}
+				<div className="caption">
+					<p> {props.data.explanation} </p>{" "}
+				</div>{" "}
+			</div>{" "}
+		</>
+	);
 }
-
