eb7103cf6b9610a96721a86e67a4c98733ce57e6
darkwolfxj <hesitation.dotdot@gmail.com>
2020-01-22 20:05:34 -0500

    stretched

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -3,16 +3,16 @@ import React from "react";
 export default function Photo(props) {
 	return (
 		<>
-			<p className="headline"> {props.data.title} </p>{" "}
+			<p className="headline"> {props.data.title}</p>
 			<img className="photo" src={props.data.hdurl} alt="nasa apod" />
 			<div className="copyright-caption">
 				<div className="copyright-date">
-					<p> {props.data.copyright} </p> <p> {props.data.date} </p>{" "}
-				</div>{" "}
+					<p> {props.data.copyright} </p> <p> {props.data.date}</p>
+				</div>
 				<div className="caption">
-					<p> {props.data.explanation} </p>{" "}
-				</div>{" "}
-			</div>{" "}
+					<p> {props.data.explanation} </p>
+				</div>
+			</div>
 		</>
 	);
 }
--- src/Components/PhotoRender.js
+++ src/Components/PhotoRender.js
@@ -1,18 +1,33 @@
-import React, {useState, useEffect} from 'react';
-import Photo from './Photo';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import Photo from "./Photo";
+import axios from "axios";
 
 export default function PhotoRender() {
-    const [data, setData]=useState({});
-    useEffect(() => {
-        axios
-            .get('https://api.nasa.gov/planetary/apod?api_key=QKaxrNzmkBuK7U6fww4gt7HPpRhQbn2YJkpAlFj1')
-            .then(response => setData(response.data))
-            .catch(error=>console.log('Data was not returned', error))},[])
+	const [data, setData] = useState({});
+	var today = new Date();
+	var Today =
+		today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+	const [date, setDate] = useState(Today);
 
-    return (
-        <div className='photo-container'>
-            <Photo data={data}/>
-        </div>
-    )
+	useEffect(() => {
+		axios
+			.get(
+				`https://api.nasa.gov/planetary/apod?api_key=QKaxrNzmkBuK7U6fww4gt7HPpRhQbn2YJkpAlFj1&date=${date}`
+			)
+			.then((response) => setData(response.data))
+			.catch((error) => console.log("Data was not returned", error));
+	}, [date]);
+	const handleChange = (e) => {
+		console.log(e.target.value);
+		setDate(e.target.value);
+	};
+	return (
+		<div className="photo-container">
+			<Photo data={data} />
+			<form>
+				<label for="date">Choose a Day</label>
+				<input id="date" type="date" onChange={handleChange} value={date} />
+			</form>
+		</div>
+	);
 }
