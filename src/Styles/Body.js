d07a070cf33376446d651d048c09e54eae30c4ae
darkwolfxj <hesitation.dotdot@gmail.com>
2020-01-23 18:02:13 -0500

    Cleaned up

--- src/Styles/Body.js
+++ src/Styles/Body.js
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 
-const Body = styled.body`
+const Body = styled.div`
 	background-image: url("http://tiny.cc/p9q3iz");
 	background-size: cover;
 	margin: 0, 10%;
+	color: white;
 `;
 export default Body;
